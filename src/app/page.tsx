a9a5bbe05d6920d26d78a21f2118483d6051d13c
ajayshsh7 <ajayshsh7@gmail.com>
2025-07-01 21:06:29 +0530

    setting up sample template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <h1>Supercharge Your Next.js Development</h1>
       <p>A VS Code Extension that lets you instantly scaffold production-ready Next.js components, layouts, and pages.</p>
     </main>
